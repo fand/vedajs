0e8e8a87ec6f45093365848128edf29eff596056
fand <fand.gmork@gmail.com>
2018-04-22 16:44:26 +0900

    Load JSON models

--- src/model-loader.ts
+++ src/model-loader.ts
@@ -19,6 +19,8 @@ export default class ModelLoader {
 
     private objLoader = new THREE.OBJLoader();
     private mtlLoader = new MTLLoader();
+    private objectLoader = new THREE.ObjectLoader();
+    private jsonLoader = new THREE.JSONLoader();
 
     async load(model: IPassModel): Promise<THREE.Object3D> {
         const url = model.PATH;
@@ -29,6 +31,60 @@ export default class ModelLoader {
             return Promise.resolve(cache.obj);
         }
 
+        let obj;
+        if (/\.obj\/?$/.test(url)) {
+            obj = await this.loadObjAndMtl(model);
+        } else if (/\.js(on)?\/?$/.test(url)) {
+            obj = await this.loadJson(model);
+        } else {
+            throw new TypeError('Unsupported model URL: ' + url);
+        }
+
+        obj = this.fixObj(obj);
+        this.cache[url] = { url, obj };
+        console.log(obj);
+        return obj;
+    }
+
+    private async loadJson(model: IPassModel): Promise<THREE.Object3D> {
+        const obj = await Promise.race([
+            new Promise<THREE.Object3D>((resolve, reject) => {
+                this.jsonLoader.load(
+                    model.PATH,
+                    (geometry, materials) => {
+                        const material = materials[0];
+                        resolve(new THREE.Mesh(geometry, material));
+                    },
+                    undefined,
+                    reject,
+                );
+            }),
+            new Promise<THREE.Object3D>((resolve, reject) => {
+                this.objectLoader.load(model.PATH, resolve, undefined, reject);
+            }),
+
+            // timeout
+            new Promise<THREE.Object3D>((_resolve, reject) => {
+                setTimeout(() => reject('Request Timeout'), 50000);
+            }),
+        ]);
+
+        if (
+            obj instanceof THREE.Mesh &&
+            obj.geometry instanceof THREE.Geometry
+        ) {
+            obj.geometry = new THREE.BufferGeometry().fromGeometry(
+                obj.geometry,
+            );
+        }
+
+        const group = new THREE.Group();
+        group.add(obj);
+
+        return group;
+    }
+
+    private async loadObjAndMtl(model: IPassModel): Promise<THREE.Object3D> {
         if (model.MATERIAL) {
             const materials = await this.loadMtl(model.MATERIAL);
             console.log(materials);
@@ -38,9 +94,30 @@ export default class ModelLoader {
             this.objLoader.setMaterials(null as any);
         }
 
-        const obj = await this.loadObj(model.PATH);
+        return this.loadObj(model.PATH);
+    }
 
-        let box: THREE.Box3;
+    private loadMtl(url: string): Promise<THREE.MaterialCreator> {
+        const match = url.match(/^(.*\\)(.*)$/) || url.match(/^(.*\/)(.*)\/?$/); // windows local file // other
+        if (!match) {
+            return Promise.reject(new TypeError('Invalid URL: ' + url));
+        }
+
+        const [, path, basename] = match;
+        this.mtlLoader.setPath(path);
+        return new Promise((resolve, reject) => {
+            this.mtlLoader.load(basename, resolve, undefined, reject);
+        });
+    }
+
+    private loadObj(url: string): Promise<THREE.Group> {
+        return new Promise((resolve, reject) => {
+            this.objLoader.load(url, resolve, undefined, reject);
+        });
+    }
+
+    private fixObj(obj: THREE.Object3D) {
+        let box: THREE.Box3 | null = null;
         obj.traverse(o => {
             if (
                 o instanceof THREE.Mesh &&
@@ -48,13 +125,18 @@ export default class ModelLoader {
             ) {
                 o.geometry.computeBoundingBox();
 
-                if (!box) {
+                if (box === null) {
                     box = o.geometry.boundingBox;
                 } else {
                     box.union(o.geometry.boundingBox);
                 }
             }
         });
+
+        // Return if children is empty
+        if (box === null) {
+            return obj;
+        }
 
         const sphere = new THREE.Sphere();
         box!.getBoundingSphere(sphere);
@@ -72,27 +154,6 @@ export default class ModelLoader {
             }
         });
 
-        this.cache[url] = { url, obj };
-
         return obj;
-    }
-
-    private loadMtl(url: string): Promise<THREE.MaterialCreator> {
-        const match = url.match(/^(.*\\)(.*)$/) || url.match(/^(.*\/)(.*)\/?$/); // windows local file // other
-        if (!match) {
-            return Promise.reject(new TypeError('Invalid URL: ' + url));
-        }
-
-        const [_, path, basename] = match;
-        this.mtlLoader.setPath(path);
-        return new Promise((resolve, reject) => {
-            this.mtlLoader.load(basename, resolve, undefined, reject);
-        });
-    }
-
-    private loadObj(url: string): Promise<THREE.Group> {
-        return new Promise((resolve, reject) => {
-            this.objLoader.load(url, resolve, undefined, reject);
-        });
     }
 }
